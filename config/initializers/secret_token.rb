760dbf68cad0a84a635beb74b23c1e7020d60f07
Tony Jiang <tjiang@yammer-inc.com>
2013-11-16 16:56:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YammerTrending::Application.config.secret_key_base = '58939c1c2a8091ba114b0a4e2b7311e900683a55c4287309b3e504d5c65478ceeb6af6759fddd26001e2475eae50a401cccda3b940f8e236742dc4a061d52acb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YammerTrending::Application.config.session_store :cookie_store, key: '_yammer_trending_session'
